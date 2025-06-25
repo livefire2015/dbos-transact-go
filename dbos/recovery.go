7a2ec6c722460069ecf8aa787c168078930061f4
maxdml <maxdml@dbos.dev>
2025-06-24 14:55:37 -1000

    add recovery file and test util

---
+++ dbos/recovery.go
@@ -0,0 +1,47 @@
+package dbos
+
+import (
+	"context"
+	"fmt"
+)
+
+func recoverPendingWorkflows(ctx context.Context, executorIDs []string) ([]WorkflowHandle[any], error) {
+	workflowHandles := make([]WorkflowHandle[any], 0)
+	// List pending workflows for the executors
+	pendingWorkflows, err := getExecutor().systemDB.ListWorkflows(ctx, ListWorkflowsDBInput{
+		Status:             WorkflowStatusPending,
+		ExecutorIDs:        executorIDs,
+		ApplicationVersion: APP_VERSION,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, workflow := range pendingWorkflows {
+		fmt.Println("Recovering workflow:", workflow.ID, "Name:", workflow.Name, "Input:", workflow.Input)
+		// TODO: handle clearing queue assignment if needed, and append a polling handle
+		if workflow.QueueName != nil {
+			continue
+		}
+		registeredWorkflow, exists := registry[workflow.Name]
+		if !exists {
+			fmt.Println("Error: workflow function not found in registry:", workflow.Name)
+			continue
+		}
+
+		// Rebuild the parameters
+		params := WorkflowParams{
+			WorkflowID: workflow.ID,
+			Timeout:    workflow.Timeout,
+			Deadline:   workflow.Deadline,
+		}
+
+		handle, err := registeredWorkflow(ctx, params, workflow.Input)
+		if err != nil {
+			fmt.Println("Error recovering workflow:", err)
+		}
+		workflowHandles = append(workflowHandles, handle)
+	}
+
+	return workflowHandles, nil
+}
